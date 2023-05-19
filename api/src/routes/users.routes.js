a59b1f31162250479e0c7b4e0566107e288c8d02
Daniel Náger Carpio <dnagerc@inscastellet.cat>
2023-05-19 16:30:27 +0200

    Adding login to users

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -14,6 +14,6 @@ router.get("/user/:id", getUser);
 router.post("/users", createUser);
 router.put("/user/:id", updateUser);
 router.delete("/user/:id", deleteUser);
-router.delete("/user/:id", loginUser);
+router.get("/userlogin", loginUser);
 
 export default router;
